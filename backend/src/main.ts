dfc7f74b978c2a2eb0a2c5e0d56e1d2e427e1fce
Donoma2 <dorianlevee@gmail.com>
2023-11-21 10:09:11 +0100

    adding .env file for backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+const port: string = process.env.BACKEND_PORT
+
+console.log('localhost:' + port);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
