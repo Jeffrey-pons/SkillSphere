1153d1e65f297ebe971edfd42e49cd469f79a2a6
Dorian Levée <dorianlevee30@gmail.com>
2023-11-24 10:22:38 +0100

    fix: minor fix to correct some problems

--- backend/src/categories/categories.service.ts
+++ backend/src/categories/categories.service.ts
@@ -12,6 +12,7 @@ export class CategoriesService {
     private categoriesRepository: Repository<Category>,
   ) {}
   create(createCategoryDto: CreateCategoryDto): void {
+    console.log('test');
     this.categoriesRepository.save(createCategoryDto);
   }
 
