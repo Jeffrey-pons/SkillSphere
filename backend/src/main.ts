31b8f803e6826a049544ef539bf669e8a12f8133
tgentreau <gentreau.thomas@gmail.com>
2023-12-04 18:08:37 +0100

    feat: mep helmet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 import dotenv from 'dotenv';
 
@@ -11,6 +12,7 @@ console.log(`${host}:${port}`);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(helmet());
   await app.listen(port, host);
 }
 bootstrap();
